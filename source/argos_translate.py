e4e0649cb5ab75940359baab0e84b183daf09ae8
MeteorClear <tivics9973@gmail.com>
2025-02-28 16:09:04 +0900

    add comments

--- source/argos_translate.py
+++ source/argos_translate.py
@@ -3,17 +3,26 @@ import argostranslate.package
 import argostranslate.translate
 
 '''
-Source file for Argos translation
-Used for offline translation
-Argos Translate is Open-source offline translation library written in Python
+Source file for Argos translation.
+Used for offline translation.
+Argos Translate is Open-source offline translation library written in Python.
 https://github.com/argosopentech/argos-translate
 '''
 
-# global flag: check for installed language pack
+# Global flag: check for installed language pack.
 VALID_CHECK = False
 
 
 def find_package_index(package_language: str) -> list:
+    """
+    Find the indexes of available language packages that match the given language string.
+
+    Args:
+        package_language (str): The language string to search for in the available packages.
+
+    Returns:
+        list: A list of indexes where the available package's name includes the package_language.
+    """
     available_packages = argostranslate.package.get_available_packages()
     package_index = []
 
@@ -26,7 +35,16 @@ def find_package_index(package_language: str) -> list:
 
 def valid_check(package_language: str= None, \
                 install_all: bool= False) -> None:
+    """
+    Validate and install the required language packages for translation.
 
+    Args:
+        package_language (str, optional): The language for which packages should be installed.
+        install_all (bool, optional): If True, installs all available language packages.
+
+    Raises:
+        Exception: If an error occurs during the package installation process.
+    """
     global VALID_CHECK
 
     try:
@@ -35,16 +53,18 @@ def valid_check(package_language: str= None, \
         installed_languages = argostranslate.translate.get_installed_languages()
         installed_languages_list = list(map(str, installed_languages))
         
-        # install all package (not recommended)
+        # Install all available packages. (not recommended)
         if install_all:
             argostranslate.argospm.install_all_packages()
 
+        # Install packages for the specified language.
         elif package_language != None:
             package_index = find_package_index(package_language)
 
             for num in package_index:
                 argostranslate.package.install_from_path(available_packages[num].download())
 
+        # Default behavior: install Korean package.
         else:
             package_index = find_package_index("Korean")
 
@@ -52,7 +72,7 @@ def valid_check(package_language: str= None, \
                 for num in package_num:
                     argostranslate.package.install_from_path(available_packages[num].download())
 
-        # flag update
+        # Update global flag, installation is complete.
         VALID_CHECK = True
 
     except Exception as e:
@@ -65,7 +85,21 @@ def text_translate(text: str, \
                    dest: str, \
                    src: str, \
                    detail: bool= False) -> str:
+    """
+    Translate a given text from the source language to the destination language.
 
+    Args:
+        text (str): The text to be translated.
+        dest (str): The destination language code.
+        src (str): The source language code.
+        detail (bool, optional): If True, returns a tuple with detailed translation information
+
+    Returns:
+        str or tuple: The translated text if detail is False, otherwise a tuple with detailed translation info.
+
+    Raises:
+        Exception: If an error occurs during the translation process.
+    """
     global VALID_CHECK
 
     if not VALID_CHECK:
