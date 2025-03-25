b620bdbd568822bdecbb3d657bb45cd53427f22f
MeteorClear <tivics9973@gmail.com>
2025-03-25 19:57:38 +0900

    add todo

--- server.py
+++ server.py
@@ -15,3 +15,24 @@ RESULT_DIR.mkdir(parents=True, exist_ok=True)
 
 #app = fastapi.FastAPI()
 
+# todo: arg set, image
+def upload_image():
+    # todo: save image
+
+    # todo: cal hash
+
+    # todo: check duplication
+
+    # todo: save file info in db
+
+    try:
+        pass
+        # todo: run process
+
+        # todo: update db
+
+    except Exception as e:
+        pass
+        # todo: exception
+
+    return
