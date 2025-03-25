f720a668f792e9c0bc1c109837380074efead748
MeteorClear <tivics9973@gmail.com>
2025-03-25 20:26:02 +0900

    connect mongodb

--- server.py
+++ server.py
@@ -5,9 +5,18 @@ import hashlib
 import shutil
 from fastapi import FastAPI, UploadFile, File
 from datetime import datetime
+from pymongo import MongoClient
 
 dotenv.load_dotenv()
 
+
+MONGODB_URL = os.getenv("MONGODB_URL")
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+
+client = MongoClient(MONGODB_URL)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
 
 UPLOAD_DIR = pathlib.Path(os.getenv("UPLOAD_DIR"))
 RESULT_DIR = pathlib.Path(os.getenv("RESULT_DIR"))
@@ -26,14 +35,14 @@ def get_file_hash(file_path):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
-# todo: arg set, image
-def upload_image(image: UploadFile):
+# api
+def upload_image(image: UploadFile = File(...)):
     save_path = UPLOAD_DIR / f"{datetime.datetime().strftime('%Y%m%d_%H%M%S_%f')}_{image.filename}"
 
     with open(save_path, "wb") as buffer:
         shutil.copyfileobj(image.file, buffer)
 
-    # todo: cal hash
+    file_hash = get_file_hash(save_path)
 
     # todo: check duplication
 
